6bf82f13b2852d1b258a5ba6a8c39501d6aa1d9e
Patrick Gregorio <patrickvgregorio@gmail.com>
2013-03-20 10:37:25 +0800

    displayed all player information

--- application/views/sub/home.php
+++ application/views/sub/home.php
@@ -3,11 +3,38 @@
     $players = Helpers_Dota::getPlayerSummaries(array(86720185, 99601231, 112521018, 99459550));
     
     foreach($players as $player) {
-        var_dump($player);
-        echo "<br />";
-        echo "<br />";
-        echo "<br />";
         var_dump(Helpers_Dota::getPlayerDetail($player, 'personaname'));
+        echo "<br />";
+        var_dump(Helpers_Dota::getPlayerDetail($player, 'steamid'));
+        echo "<br />";
+        var_dump(Helpers_Dota::getPlayerDetail($player, 'communityvisibilitystate'));
+        echo "<br />";
+        var_dump(Helpers_Dota::getPlayerDetail($player, 'profilestate'));
+        echo "<br />";
+        var_dump(Helpers_Dota::getPlayerDetail($player, 'lastlogoff'));
+        echo "<br />";
+        var_dump(Helpers_Dota::getPlayerDetail($player, 'profileurl'));
+        echo "<br />";
+        var_dump(Helpers_Dota::getPlayerDetail($player, 'avatar'));
+        echo "<br />";
+        var_dump(Helpers_Dota::getPlayerDetail($player, 'avatarmedium'));
+        echo "<br />";
+        var_dump(Helpers_Dota::getPlayerDetail($player, 'avatarfull'));
+        echo "<br />";
+        var_dump(Helpers_Dota::getPlayerDetail($player, 'personastate'));
+        echo "<br />";
+        var_dump(Helpers_Dota::getPlayerDetail($player, 'realname'));
+        echo "<br />";
+        var_dump(Helpers_Dota::getPlayerDetail($player, 'primaryclanid'));
+        echo "<br />";
+        var_dump(Helpers_Dota::getPlayerDetail($player, 'timecreated'));
+        echo "<br />";
+        var_dump(Helpers_Dota::getPlayerDetail($player, 'loccountrycode'));
+        echo "<br />";
+        
+        echo "<br />";
+        echo "<br />";
+        
         echo "<br />";
     }
     ?>
