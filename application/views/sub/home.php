b379942cb4c2544de8933cd8ccf5a18f4b552278
Patrick Gregorio <patrickvgregorio@gmail.com>
2013-04-04 21:23:20 +0800

    testing the images

--- application/views/sub/home.php
+++ application/views/sub/home.php
@@ -5,11 +5,13 @@
     $match = new Dota_Match(149914404);
     $players = $match->players();
     foreach($players as $player) {
-        if($player->account_id == "86720185") {
-            var_dump($player);
-            echo "<img src=\"" . Helpers_Dota::getHeroImage($player->hero_id, false) . "\" alt=\"" . Helpers_Dota::getHeroName($player->hero_id) . "\" title=\"" . Helpers_Dota::getHeroName($player->hero_id) . "\" />";
-            echo "<img src=\"{$player->user->avatar}\" />";
-        }
+        echo "<img src=\"" . Helpers_Dota::getHeroImage($player->hero_id, false) . "\" alt=\"" . Helpers_Dota::getHeroName($player->hero_id) . "\" title=\"" . Helpers_Dota::getHeroName($player->hero_id) . "\" />";
+        echo "<br />";
+        echo "<img src=\"{$player->user->avatar}\" /> {$player->user->persona_name}";
+        echo "<br />";
+        echo "<br />";
+        echo "<br />";
+        
     }
     ?>
 </div>
