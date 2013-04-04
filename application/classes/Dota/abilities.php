f1c75f55fc160e329bf6f8cafb7e6c4e541ead34
Patrick Gregorio <patrickvgregorio@gmail.com>
2013-04-04 23:04:23 +0800

    added links to images

---
+++ application/classes/Dota/abilities.php
@@ -0,0 +1,4 @@
+<?php
+//http://media.steampowered.com/apps/dota2/images/abilities/sven_storm_bolt_hp1.png
+//http://media.steampowered.com/apps/dota2/images/abilities/sven_storm_bolt_lg.png
+?>
---
+++ application/classes/Dota/items.php
@@ -0,0 +1,3 @@
+<?php
+//http://media.steampowered.com/apps/dota2/images/items/power_treads_eg.png
+?>
