f8aa99944bb5e3782695b433c26df10c534b15ec
Patrick Gregorio <patrickvgregorio@gmail.com>
2013-04-17 17:54:55 +0800

    added a timeout for CURL

--- application/classes/Helpers/util.php
+++ application/classes/Helpers/util.php
@@ -85,8 +85,9 @@ class Helpers_Util {
     public static function curl($url, $exec = false) {
         $ch = curl_init();
 
-        curl_setopt ($ch, CURLOPT_URL, $url);
-        curl_setopt ($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 12000);
         
         if(!$exec) {
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -96,8 +97,8 @@ class Helpers_Util {
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         }
 
-        $response = curl_exec ($ch);
-        curl_close ($ch);
+        $response = curl_exec($ch);
+        curl_close($ch);
         
         return $response;
     }
